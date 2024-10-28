813ccec63f30b258880c505e60bb909b86374fcc
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-28 15:36:19 +0000

    Update display when pressing digit buttons

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const display = document.querySelector('.display');
+const buttonsContainer = document.querySelector('.buttons');
+
+let displayContent = display.innerText;
+
 function add(a, b) {
     return a + b;
 }
@@ -22,3 +27,22 @@ function operate(a, operator, b) {
         case '/': return divide(a, b);
     }
 }
+
+buttonsContainer.addEventListener('click', event => {
+    if (event.target === buttonsContainer) return;
+
+    const button = event.target;
+    const isDigit = button.classList.contains('digit');
+
+    if (isDigit) {
+        const digit = button.innerText;
+
+        if (displayContent === '0') {
+            displayContent = '';
+        }
+
+        displayContent += digit;
+    }
+
+    display.innerText = displayContent;
+})
